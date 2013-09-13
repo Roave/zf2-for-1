c157bf47ed19b33b83d2f46607cd52ac0b6d0aae
Aleksey Khudyakov <aleksey@xerkus.pro>
2013-09-14 04:46:09 +1100

    Add Module class

---
+++ Module.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zf2for1;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements
+    AutoloaderProviderInterface,
+    ConfigProviderInterface,
+    ServiceProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig($env = null)
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
